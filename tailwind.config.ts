5fa363cef8f995154cb158a5450488d83212feef
Kimd0ng <kdongh0406@gmail.com>
2025-03-25 22:14:41 +0900

    feat: Header Component Publishing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        rotateBorder: "rotateBorder 4s linear infinite",
+      },
+      keyframes: {
+        rotateBorder: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
