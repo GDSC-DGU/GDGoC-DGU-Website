0e205dfc5b20344487b784e6468505afe421c056
Kimd0ng <kdongh0406@gmail.com>
2025-03-26 18:58:35 +0900

    feat: Footer init

--- src/components/Footer.component.tsx
+++ src/components/Footer.component.tsx
@@ -5,7 +5,9 @@
 
 export const Footer = () => {
   return (
-    <div className='p-4 text-center justify-center text-xs text-neutral-500 border-t border-neutral-100'>
+    <div className='p-4 text-start justify-start text-xs text-neutral-500 border-t border-neutral-100'>
+      <p>About Us</p>
+      <p>Contact</p>
       <p className='mt-2'>
         Copyright © GDGoC DGU - GDGoC DGU <br /> All Rights Reserved.
       </p>
