297fd0057da52b4b7a77907a421e7307b4ac75ea
Taew00k <davis0625@dgu.ac.kr>
2025-04-05 00:52:32 +0900

    Feat: CardFlip Component Publishing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -134,8 +134,8 @@ const config: Config = {
     screens: {
       tablet: "768px",
       // => @media (min-width: 768px) { ... }
-      desktop: "1440px",
-      // => @media (min-width: 1440px) { ... }
+      desktop: "1024px",
+      // => @media (min-width: 1024px) { ... }
     },
   },
   plugins: [require("@tailwindcss/typography"), addVariablesForColors],
