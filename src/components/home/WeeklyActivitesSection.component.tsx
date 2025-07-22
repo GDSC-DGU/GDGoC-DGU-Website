b82faeb33760afcb4ac65b6bb9b9e8f816b2d209
Kimd0ng <kdongh0406@gmail.com>
2025-07-22 22:22:39 +0900

    ✨ feat: Add home page sections including Introduce, Weekly Activities, Projects, Seminars, Studies, and Techblog components with SVG image integration

---
+++ src/components/home/WeeklyActivitesSection.component.tsx
@@ -0,0 +1,27 @@
+import {
+  WeeklyProjectSection,
+  WeeklySeminarSection,
+  WeeklyStudySection,
+  WeeklyTechblogSection,
+} from "@/src/components/home";
+import { TwoCircleComponent } from "@/src/components/page/TwoCircle.components";
+
+export const WeeklyActivitesSection = () => {
+  return (
+    <section className='flex flex-col items-center gap-8 p-10'>
+      <TwoCircleComponent
+        TwoCircle={{
+          circleColor: "border-blue",
+          text: "Weekly Activities",
+          textColor: "text-blue",
+          size: 230,
+          textSize: 20,
+        }}
+      />
+      <WeeklySeminarSection />
+      <WeeklyProjectSection />
+      <WeeklyStudySection />
+      <WeeklyTechblogSection />
+    </section>
+  );
+};
