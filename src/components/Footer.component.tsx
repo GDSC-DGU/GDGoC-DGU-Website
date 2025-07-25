814155113f1a926b4f57a4001198c64cb4994735
Kimd0ng <kdongh0406@gmail.com>
2025-07-26 01:23:58 +0900

    ✨ feat: Revamp Footer component with new layout and contact information

--- src/components/Footer.component.tsx
+++ src/components/Footer.component.tsx
@@ -1,16 +1,38 @@
 /**
- * 모든 화면에  적용되는 Footer 컴포넌트입니다.
- * @author 도형
+ * 모든 화면에 적용되는 Footer 컴포넌트입니다.
+ * @author 동현
  */
 
 export const Footer = () => {
   return (
-    <div className='p-4 text-start justify-start text-xs text-neutral-500 border-t border-neutral-100'>
-      <p>About Us</p>
-      <p>Contact</p>
-      <p className='mt-2'>
-        Copyright © GDGoC DGU - GDGoC DGU <br /> All Rights Reserved.
-      </p>
-    </div>
+    <footer className='bg-black text-white p-8'>
+      <div className='max-w-6xl mx-auto space-y-8'>
+        {/* About Us Section */}
+        <div className='space-y-2'>
+          <h3 className='text-xl font-bold'>About Us</h3>
+          <p className='text-sm text-gray-300'>Final Update 2025.07.26</p>
+        </div>
+
+        {/* Contact Section */}
+        <div className='space-y-2'>
+          <h3 className='text-xl font-bold'>Contact</h3>
+          <div className='text-sm text-gray-300 space-y-1'>
+            <div className='flex'>
+              <span className='w-16'>이메일</span>
+              <span className='ml-4'>gdsc.dgu@gmail.com</span>
+            </div>
+            <div className='flex'>
+              <span className='w-16'>주소</span>
+              <span className='ml-4'>서울특별시 중구 필동로1길 30</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Copyright Section */}
+        <div className='pt-4'>
+          <p className='text-sm text-gray-300'>Copyright © GDSC DGU</p>
+        </div>
+      </div>
+    </footer>
   );
 };
